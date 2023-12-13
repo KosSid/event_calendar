267beaeca5d8e11b3853b7f96d5142eea272fe86
kossid <koskossid@gmail.com>
2023-12-13 17:58:49 +0100

    routes_were added

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 function Button({ children, handleClick }: ButtonProps) {
   return (
     <button
-      className=" bg-slate-300 p-2 mx-2 cursor-pointer hover:bg-slate-400 rounded-xl flex items-center justify-center"
+      className=" bg-slate-300 p-2 mx-2 cursor-pointerflex items-center justify-center"
       onClick={handleClick}
     >
       {children}
--- src/Components/EventCalendar.tsx
+++ src/Components/EventCalendar.tsx
@@ -2,11 +2,18 @@ import EventCalendarTitle from './EventCalendarTitle';
 import EventCalendarTitleWeekDays from './EventCalendarTitleWeekDays';
 import EventCalendarMonthDates from './EventCalendarMonthDates';
 
-function EventCalendar() {
-  const currentDate = new Date();
+interface EventCalendarProps {
+  currentDate: Date;
+  setCurrentDate: React.Dispatch<React.SetStateAction<Date>>;
+}
+
+function EventCalendar({ currentDate, setCurrentDate }: EventCalendarProps) {
   return (
     <div className="container mx-auto p-4 max-w-4xl">
-      <EventCalendarTitle currentDate={currentDate} />
+      <EventCalendarTitle
+        currentDate={currentDate}
+        setCurrentDate={setCurrentDate}
+      />
       <EventCalendarTitleWeekDays />
       <EventCalendarMonthDates currentDate={currentDate} />
     </div>
